8a4802dfe6cecb4afb104983d70af0625f9f3e8f
nsheff <nsheff@users.noreply.github.com>
2023-08-28 21:36:22 -0400

    some random exploring

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import forge from 'node-forge';
-
+import canonicalize from 'canonicalize';
+import fastaParser from 'fasta-js';
+import React from 'react';
 // const products = [
 //   { name: 'Cabbage', id: 1 },
 //   { name: 'Garlic', id: 2 },
 //   { name: 'Apple', id: 3 },
 // ];
 
+var options = {
+  'definition': 'id',
+  'delimiter': '>'
+};
+
+var fp = new fastaParser(options);
 const products = [];
 
 function trunc512_digest(seq, offset=24) {
@@ -22,7 +30,44 @@ function trunc512_digest(seq, offset=24) {
   return bytes
 }
 
+function seqcol_digest(csc) {
+  var csc2 = {}
+  for (var key in csc) {
+    csc2[key] = trunc512_digest(canonicalize(csc[key]))
+  }
+  return trunc512_digest(canonicalize(csc2))
+}
+
 console.log(trunc512_digest("TCGA"))
+var demo = {
+  "lengths": [
+    8,
+    4,
+    4
+  ],
+  "names": [
+    "chrX",
+    "chr1",
+    "chr2"
+  ],
+  "sequences": [
+    "5f63cfaa3ef61f88c9635fb9d18ec945",
+    "31fc6ca291a32fb9df82b85e5f077e31",
+    "92c6a56c9e9459d8a42b96f7884710bc"
+  ],
+  "names_lengths": [
+    "2914fff9858465fd1facf69b776c3380",
+    "71083cf3401cf5a0fa604dfec95c611c",
+    "843410ce56006323efe493f96e90b05d"
+  ]
+}
+var sequenceData = `>gi|123456|Sequence A
+ATCGATCGATCG
+>gi|567890|Sequence B
+CATCATCATGGG`
+
+console.log(seqcol_digest(demo))
+console.log(fp.parse(sequenceData));
 
 // const listItems = products.map(product =>
 //   <li key={product.id}>
@@ -49,12 +94,66 @@ function MyListToShow(myListItems) {
     )
 }
 
+function MyTextbox() {
+  return (
+    <>
+    <input type="file" id="myFile" name="filename" />
+    </>
+  )
+}
+
 const getData = async () => {
   // const resp = await fetch('http://127.0.0.1:8100/collection/59319772d1bcf2e0dd4b8a296f2d9682?format=collated');  # local dev
   const resp = await fetch('https://seqcolapi.databio.org/collection/59319772d1bcf2e0dd4b8a296f2d9682?format=collated');
   const json = await resp.json();
   // return products
   return json
+}
+
+class FileInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.uploadFile = this.uploadFile.bind(this);
+  }
+  
+  uploadFile(event) {
+      let file = event.target.files[0];
+      console.log(file);
+      
+      if (file) {
+        let data = new FormData();
+        data.append('file', file);
+        // axios.post('/files', data)...
+
+        let reader = new FileReader();
+        reader.readAsText(file);
+
+        reader.onload = function() {
+          console.log(reader.result);
+        };
+      }
+  }
+  
+  render() {
+    return <span>
+      <input type="file"
+      name="myFile"
+      onChange={this.uploadFile} />
+    </span>
+  }
+}
+
+
+function parseFasta() { 
+  var fastaFile = document.getElementById("myTextarea").value;
+  // read in file content
+
+
+
+  var parsed = fp.parse(fasta);
+  console.log(parsed);
+  var result = document.getElementById("result");
+  result.innerHTML = seqcol_digest(parsed);
 }
 
 function App() {
@@ -69,34 +168,20 @@ function App() {
     });
   }
 
-
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       <ul><MyListToShow myListItems={myListItems} /></ul>
       <button onClick={handleClick}>
         Load data
       </button>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <br />
+      <MyTextbox />
+      <br />
+      <button onClick={parseFasta}>
+        Compute seqcol identifier
+      </button>
+      <FileInput />
+      <div className="card" id="result"></div>
     </>
   )
 }
